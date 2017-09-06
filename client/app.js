7e1aba1f9aa945771c20c83cada04f6537b24b2d
Hongfei Wu <hongfei.wu@citrix.com>
2017-09-06 22:25:45 +0800

    Fix ArrayBuffer to base64 stack leak issue

--- client/app.js
+++ client/app.js
@@ -15,19 +15,34 @@
             var context = canvas.getContext('2d');
             var img = new Image();
 
-            canvas.width = camera.width;
-            canvas.height = camera.height;
-
+            if (camera.width > window.innerWidth/2) {
+                canvas.width = camera.width/2;
+                canvas.height = camera.height/2;
+            } else {
+                canvas.width = camera.width;
+                canvas.height = camera.height;
+            }
+            
             context.fillStyle = '#333';
-            context.fillText('Loading...', camera.width / 2 - 30, camera.height / 3);
+            context.fillText('Loading...', canvas.width / 2 - 30, canvas.height / 3);
 
             socket.on('frame'+camera.id, function (data) {
                 var uint8Arr = new Uint8Array(data.buffer);
-                var str = String.fromCharCode.apply(null, uint8Arr);
+                var str = '';
+
+                var len = uint8Arr.byteLength;
+                for (var i = 0; i < len; i++) {
+                    str += String.fromCharCode(uint8Arr[i]);
+                }
+
                 var base64String = btoa(str);
 
                 img.onload = function () {
-                    context.drawImage(this, 0, 0, camera.width, camera.height);
+                    if (camera.width > window.innerWidth/2) {
+                        context.drawImage(this, 0, 0, camera.width/2, camera.height/2);                        
+                    } else {
+                        context.drawImage(this, 0, 0, camera.width, camera.height);
+                    }
                 };
                 img.src = 'data:image/png;base64,' + base64String;
             });
